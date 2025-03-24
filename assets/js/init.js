c484a08af4a22ace8a6b4af0fc60c572c641c76e
Jorge Serrano Pérez <jorge.serrano.perez@gmail.com>
2025-03-24 15:45:07 +0100

    Contact Form

--- assets/js/init.js
+++ assets/js/init.js
@@ -153,54 +153,7 @@ jQuery(document).ready(function($) {
 ------------------------------------------------------*/
 
    $('form#contactform button.submit').click(function() {
-
-      alert('ey!');
       document.getElementById("contactform").submit()
-
-      // $('#image-loader').fadeIn();
-
-      // var contactName = $('#contactForm #contactName').val();
-      // var contactEmail = $('#contactForm #contactEmail').val();
-      // var contactSubject = $('#contactForm #contactSubject').val();
-      // var contactMessage = $('#contactForm #contactMessage').val();
-
-      // var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-      //          '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      // $.ajax({
-
-	   //    type: "POST",
-	   //    url: "inc/sendEmail.php",
-	   //    data: data,
-	   //    success: function(msg) {
-
-      //       // Message was sent
-      //       if (msg == 'OK') {
-      //          $('#image-loader').fadeOut();
-      //          $('#message-warning').hide();
-      //          $('#contactForm').fadeOut();
-      //          $('#message-success').fadeIn();   
-      //       }
-      //       // There was an error
-      //       else {
-      //          $('#image-loader').fadeOut();
-      //          $('#message-warning').html(msg);
-	   //          $('#message-warning').fadeIn();
-      //       }
-
-	   //    }
-
-      // });
-   //    return false;
    });
 
-
 });
-
-
-
-
-
-
-
-
